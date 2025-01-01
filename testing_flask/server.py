035cabc4204506a50b93b8292e846bae11f50940
Regdon <128311045+Regdon@users.noreply.github.com>
2025-01-01 14:23:58 +0000

    Uncomment code

--- testing_flask/server.py
+++ testing_flask/server.py
@@ -12,27 +12,27 @@ Device.pin_factory = factory
 
 print(Device.pin_factory)
 
-# led = LED(26)
+led = LED(26)
 
-# app = Flask(__name__)
-# socketio = SocketIO(app)
+app = Flask(__name__)
+socketio = SocketIO(app)
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# @socketio.on('button_pressed')
-# def handle_button_press(data): 
-#     print(f"Button pressed: {data}")  # Receive and print the button's value
+@socketio.on('button_pressed')
+def handle_button_press(data): 
+    print(f"Button pressed: {data}")  # Receive and print the button's value
 
-#     if data == 1:
-#         led.on()
-#     else:
-#         led.off()
+    if data == 1:
+        led.on()
+    else:
+        led.off()
 
-#     emit('response', {'message': f"Button {data} pressed!"})
+    emit('response', {'message': f"Button {data} pressed!"})
 
-# if __name__ == '__main__':
-#     socketio.run(app, debug=True, host='192.168.1.207', port=5000)
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='192.168.1.207', port=5000)
 
-#     #app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
