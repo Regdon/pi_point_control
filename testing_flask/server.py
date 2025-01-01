12f21a7e027e79435513740f3e0bc878b44e77b6
Regdon <128311045+Regdon@users.noreply.github.com>
2025-01-01 13:54:30 +0000

    spelling fix

--- testing_flask/server.py
+++ testing_flask/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 #import RPi.GPIO as GPIO
-from gpiozero inport LED
+from gpiozero import LED
 # GPIO.setmode(GPIO.BCM)
 # GPIO.setup(580, GPIO.OUT)
 
