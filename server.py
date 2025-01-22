1e70a670612754dc3ecaa8a248b3b6165a75d351
Mark <marks83@hotmail.co.uk>
2025-01-22 19:08:01 +0000

    Changed index to home

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
 
 @app.route('/')
-def index():
+def home():
     data = {'message': 'Hello, World!'}
     return render_template('index.html', data=data)
 
