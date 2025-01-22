801164f0bf216e8eb88eb3e3ba818c2e9531d935
Mark <marks83@hotmail.co.uk>
2025-01-22 20:11:45 +0000

    Added start and end path

--- static/script.js
+++ static/script.js
@@ -1,9 +1,10 @@
 function drawLine(ctx, x1, y1, x2, y2, colour) {
     ctx.strokeStyle = colour;
-    console.log(colour);
+    ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
+    ctx.closePath();
 };
 
 document.addEventListener('DOMContentLoaded', function () {
