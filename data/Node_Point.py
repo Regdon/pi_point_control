bbfc3420715787e27c53cd9f204afb31b7c180d8
Mark <marks83@hotmail.co.uk>
2025-01-22 20:41:28 +0000

    Init fix

--- data/Node_Point.py
+++ data/Node_Point.py
@@ -2,7 +2,7 @@ from .Node import Node
 
 class Node_Point(Node):
     def __init__(self, id, x, y, parent, child_id_1, child_id_2):
-        Node().__init__(self, id, x, y, parent)
+        Node.__init__(self, id, x, y, parent)
 
         self.children = ["", child_id_1, child_id_2]
         self.point_state = 1
