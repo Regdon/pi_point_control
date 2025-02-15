754c04778a8d87d2f8ed272fd085834751f953db
Regdon <marks83@hotmail.co.uk>
2025-02-15 15:27:35 +0000

    Worked out which way to set points for route

--- data/route.py
+++ data/route.py
@@ -1,3 +1,7 @@
+from .Node_Point import Node_Point
+
+import static
+
 class Route:
     def __init__(self, data):
         self.id = data["id"]
@@ -9,13 +13,40 @@ class Route:
         self.colour = data["colour"]
 
         self.route = []
+        self.routeStates = []
 
     def SetupRoute(self, engine):
         self.route = engine.GetRoute(self.node_id_start, self.node_id_end)
+
         s = f"route from {self.node_id_start} to {self.node_id_end}:"
         for node in self.route:
             s += f"{node.id}, "
         print(s)
     
+        for node in self.route:
+            if isinstance(node, Node_Point):
+                if self.NodeInRoute(node.set_straight_id):
+                    obj = RouteState(node, static.POINT_STATE_STRAIGHT)
+                    self.routeStates.append(obj)
+                    print(f"Node ID {node.id} must be set straight for this route")
+                elif self.NodeInRoute(node.set_turnout_id):
+                    obj = RouteState(node, static.POINT_STATS_TURNOUT)
+                    self.routeStates.append(obj)
+                    print(f"Node ID {node.id} must be set turnout for this route")
 
+    def NodeInRoute(self, node_id):
+        for node in self.route:
+            if (node.id == node_id):
+                return True
+        return False
 
+    def SetRoute(self):
+        pass
+
+    def ClearRoute(self):
+        pass
+
+class RouteState:
+    def __init__(self, node, state):
+        self.node = node
+        self.state = state
