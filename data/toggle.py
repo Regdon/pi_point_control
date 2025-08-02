66ec27c9dfdc927cd91e326898f4b9efd49a6703
Regdon <marks83@hotmail.co.uk>
2025-08-02 15:25:36 +0100

    Toggle logic

---
+++ data/toggle.py
@@ -0,0 +1,75 @@
+from .Node_Point import Node_Point
+
+import static
+
+class Toggle:
+    def __init__(self, data, engine):
+        # id
+        if "id" in data:
+            self.id = data["id"]
+        else:
+            self.id = ""
+        self.engine = engine
+
+        # button properties
+        if "button_position_x" in data:
+            self.button_position_x = data["button_position_x"]
+        else:
+            self.button_position_x = 0
+            
+        if "button_position_y" in data:
+            self.button_position_y = data["button_position_y"]
+        else:
+            self.button_position_y = 0
+            
+        if "button_colour_defaut" in data and "button_colour_toggle" in data:
+            self.button_colour = [data["button_colour_default"], data["button_colour_toggle"]]
+        else:
+            self.button_colour = ["#000000", "#000000"]
+
+        # toggle properties
+        self.state = 0
+        self.toggle_points = []
+
+    def AddNode(self, node_id, turnout_state):
+        node = self.engine.GetNodeByID(node_id)
+        item = ToggleItem(node, turnout_state)
+        self.toggle_points.append(item)
+
+    def Toggle(self):        
+        if (self.state == 0):
+            self.state = 1
+        else:
+            self.state = 0
+        
+        for point in self.toggle_points:
+            point.setState(self.state)
+
+    def GetButtonJSON(self):
+        pass
+
+    def GetColour(self):
+        pass
+
+    def IsClicked(self):
+        pass
+
+
+class ToggleItem:
+    def __init__(self, node, state_toggled):
+        self.node = node
+        if (state_toggled == "Turnout"):
+            self.state_default = static.POINT_STATE_STRAIGHT
+            self.state_toggled = static.POINT_STATS_TURNOUT
+        else:
+            self.state_default = static.POINT_STATS_TURNOUT
+            self.state_toggled = static.POINT_STATE_STRAIGHT
+        self.state = 0
+
+    def setState(self, value):
+        self.state = value
+        if (self.state == 0):
+            self.node.SetPointState(self.state_default)
+        else:
+            self.node.SetPointState(self.state_toggled)
+        
